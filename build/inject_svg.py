2c30aed3ee31bdebd47fc07072ffed014ac50ce1
DIN | DKE <89450610+Admin-DIN-DKE@users.noreply.github.com>
2026-04-20 13:34:40 +0200

    Refactor path handling in inject_svg.py

--- build/inject_svg.py
+++ build/inject_svg.py
@@ -2,19 +2,18 @@ from bs4 import BeautifulSoup
 from pathlib import Path
 import sys
 
-base_dir = Path(sys.argv[1])
+base = Path(sys.argv[1])
 
-html = (base_dir / "base.html").read_text()
+html = (base / "base.html").read_text()
 soup = BeautifulSoup(html, "html.parser")
 
-svg_files = sorted(base_dir.glob("diagram*.mmd.svg"))
-code_blocks = soup.find_all("code")
+svg_files = sorted(base.glob("diagram*.mmd.svg"))
 
 i = 0
-for c in code_blocks:
-    if "mermaid" in c.text and i < len(svg_files):
+for code in soup.find_all("code"):
+    if "mermaid" in code.text and i < len(svg_files):
         svg = BeautifulSoup(svg_files[i].read_text(), "html.parser")
-        c.replace_with(svg)
+        code.replace_with(svg)
         i += 1
 
-(base_dir / "final.html").write_text(str(soup))
+(base / "final.html").write_text(str(soup))
